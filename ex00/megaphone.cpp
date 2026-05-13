4087d0a9ece44fa1132570f8f2a6be8fc77cb301
wlucas-f <wallacelfgoncalves@gmail.com>
2026-05-13 18:34:09 +0100

    fixed for cpp std 98

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,22 +5,20 @@
 
 int main(int argc, char **argv)
 {
-	if(argc < 2)
+	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
 
-	for(int count{ 1 }; count < argc; ++count)
+	for (int count = 1; count < argc; ++count)
 	{
-		std::string phrase {};
-		phrase = argv[count];
+		std::string str (argv[count]);
 		if (count != 1)
 			std::cout << ' ';
-		for (auto& x : phrase) {
-			x = toupper(x);
-		}
-		std::cout << phrase;
+		size_t len = str.length();
+		for (size_t i = 0; i < len; ++i)
+			std::cout << (char) std::toupper(str[i]);
 	}
 	std::cout << '\n';
 	return 0;
